ca21023f441d57ac52c91e853203d5722e4d81b7
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 17:57:49 -0600

    props

---
+++ [props]/protocol_benches/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Crispy'
+description 'Protocol Benches'
+version '1.2.0'
+
+data_file "DLC_ITYP_REQUEST" "stream/NKHD_Benches.ytyp"
---
+++ [props]/protocol_flags/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+description 'Protocol Flags'
+author 'Assent Studios'
+
+this_is_a_map 'yes'
