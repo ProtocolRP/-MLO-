6e2c1ea96667317395c1ed613ac3f86a9a493629
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 17:46:28 -0600

    props

---
+++ [props]/bzzz_allprops/stream/bzzz_food_bundles/data/bzzz_cotton_candy/animations_coord.lua
@@ -0,0 +1,19 @@
+-- Template
+
+	["cotton_candy_yellow"] = {"mp_player_inteat@burger", "mp_player_int_eat_burger", "Eat cotton candy yellow", AnimationOptions =
+   {
+      Prop = "bzzz_candy_cotton_yellow", --or bzzz_candy_cotton_pink, bzzz_candy_cotton_blue, bzzz_candy_cotton_red, bzzz_candy_cotton_stick
+      PropBone = 18905,
+      PropPlacement = {0.15, 0.07, 0.0, -50.0, 150.0, 0.0},
+      EmoteLoop = true,
+      EmoteMoving = true,
+   }}, 
+   
+   ["making_cotton_candy"] = {"anim@heists@humane_labs@finale@keycards", "ped_a_enter_loop", "Making cotton candy", AnimationOptions =
+   {
+       Prop = 'bzzz_candy_cotton_yellow', --or bzzz_candy_cotton_pink, bzzz_candy_cotton_blue, bzzz_candy_cotton_red, bzzz_candy_cotton_stick
+       PropBone = 18905,
+       PropPlacement = {0.27, -0.04, -0.02, 24.0, 110.0, 0.0},
+       EmoteMoving = true,
+       EmoteLoop = true
+   }},
---
+++ [props]/bzzz_allprops/stream/bzzz_food_bundles/data/bzzz_drinks/animation_coords_dpemotes.lua
@@ -0,0 +1,264 @@
+   ["bzzz_beer_a"] = {"mp_player_intdrink", "loop_bottle", "Drink big beer", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_beer_a',
+		PropBone = 18905,
+		PropPlacement = {0.1, -0.05, 0.06, -83.0, 190.0, 18.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_beer_b"] = {"mp_player_intdrink", "loop_bottle", "Drink big beer", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_beer_b',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.12, 0.07, -83.0, 190.0, 18.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_beer_c"] = {"mp_player_intdrink", "loop_bottle", "Drink small beer", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_beer_c',
+		PropBone = 18905,
+		PropPlacement = {0.06, -0.05, 0.05, -61.0, 85.0, -15.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_beer_d"] = {"mp_player_intdrink", "loop_bottle", "Drink pint of beer", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_beer_d',
+		PropBone = 18905,
+		PropPlacement = {0.06, -0.05, 0.05, -61.0, 85.0, -15.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_bahamamama_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Bahama Mama", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_bahamamama_a',
+		PropBone = 18905,
+		PropPlacement = {0.06, -0.05, 0.05, 79.0, 35.0, 145.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_bluelagoon_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Blue Lagoon", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_bluelagoon_a',
+		PropBone = 18905,
+		PropPlacement = {0.06, -0.05, 0.05, 79.0, 35.0, 145.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_longislandicedtea_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Long Island Iced Tea", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_longislandicedtea_a',
+		PropBone = 18905,
+		PropPlacement = {0.06, -0.05, 0.05, 79.0, 35.0, 145.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_sexonthebeach_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Sex on the Beach", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_sexonthebeach_a',
+		PropBone = 18905,
+		PropPlacement = {0.06, -0.05, 0.05, 79.0, 35.0, 145.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_blackrussian_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Black Russian", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_blackrussian_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, 0.0, 0.06, -75.0, 40.0, -50.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_maitai_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Mai Tai", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_maitai_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, 0.0, 0.06, -75.0, 40.0, -50.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_negroni_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Negroni", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_negroni_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, 0.0, 0.06, -75.0, 40.0, -50.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_oldfashioned_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Old Fashioned", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_oldfashioned_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, 0.0, 0.06, -75.0, 40.0, -50.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},     
+   ["bzzz_whiskeysour_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Whiskey Sour", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_whiskeysour_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, 0.0, 0.06, -75.0, 40.0, -50.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_whiterussian_a"] = {"mp_player_intdrink", "loop_bottle", "Drink White Russian", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_whiterussian_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, 0.0, 0.06, -75.0, 40.0, -50.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_bloodymary_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Bloody Mary", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_bloodymary_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, -0.08, 0.05, -83.0, 190.0, 18.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_gintonic_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Gin with Tonic", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_gintonic_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, -0.08, 0.05, -83.0, 190.0, 18.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_rumcoke_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Rum with Coke", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_rumcoke_a',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.09, 0.07, -83.0, 190.0, 28.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},    
+   ["bzzz_screwdriver_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Screwdriver", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_screwdriver_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, -0.08, 0.05, -83.0, 190.0, 18.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_tomcollins_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Tom Collins", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_tomcollins_a',
+		PropBone = 18905,
+		PropPlacement = {0.09, -0.08, 0.05, -83.0, 190.0, 18.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_cosmopolitan_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Cosmopolitan", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_cosmopolitan_a',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.01, 0.07, 27.0, 118.0, 188.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},   
+   ["bzzz_gimlet_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Gimlet", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_gimlet_a',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.01, 0.07, 27.0, 118.0, 188.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }}, 
+   ["bzzz_margarita_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Margarita", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_margarita_a',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.01, 0.07, 27.0, 118.0, 188.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }}, 
+   ["bzzz_martini_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Martini", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_martini_a',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.01, 0.07, 27.0, 118.0, 188.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }}, 
+   ["bzzz_manhattan_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Manhattan", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_manhattan_a',
+		PropBone = 18905,
+		PropPlacement = {0.08, -0.01, 0.07, 27.0, 118.0, 188.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }}, 
+   ["bzzz_shot_b52_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of B52", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_b52_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_bluekamikaze_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of Blue Kamikaze", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_bluekamikaze_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},  
+   ["bzzz_kamikaze_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of Kamikaze", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_kamikaze_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_lemondrop_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of Lemon Drop", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_lemondrop_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_rum_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of Rum", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_rum_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_vodka_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of Vodka", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_vodka_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_washinapple_a"] = {"mp_player_intdrink", "loop_bottle", "Drink a shot of Washington Apple", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_shot_washinapple_a',
+		PropBone = 18905,
+		PropPlacement = {0.14, 0.02, 0.03, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_moscowmule_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Moscow Mule", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_moscowmule_a',
+		PropBone = 18905,
+		PropPlacement = {0.1, -0.02, 0.05, -70.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
+   ["bzzz_coconut_a"] = {"mp_player_intdrink", "loop_bottle", "Drink Coconut", AnimationOptions =
+   {   
+		Prop = 'bzzz_drink_coconut_a',
+		PropBone = 18905,
+		PropPlacement = {0.1, 0.04, 0.11, -13.0, 140.0, 0.0},
+		EmoteLoop = true,
+		EmoteMoving = true,
+   }},
