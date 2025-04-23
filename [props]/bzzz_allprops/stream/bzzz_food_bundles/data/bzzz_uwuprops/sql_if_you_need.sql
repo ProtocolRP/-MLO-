795a5f9457c057c99cca8eb7a124b63829ed71e1
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 17:48:21 -0600

    props

---
+++ [props]/bzzz_allprops/stream/bzzz_food_bundles/data/bzzz_uwuprops/sql_if_you_need.sql
@@ -0,0 +1,19 @@
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('bzzz_uwuprops_lollipop_a', 'Uwu lollipop', 1),
+	('bzzz_uwuprops_popsicle_a', 'Uwu popsicle', 1),
+    ('bzzz_uwuprops_donut_a', 'Uwu donut', 1),
+	('bzzz_uwuprops_cake_a', 'Uwu cake', 1),
+	('bzzz_uwuprops_icecream_a', 'Uwu icecream', 1),
+	('bzzz_uwuprops_wafer_a', 'Uwu wafer', 1),	
+	('bzzz_uwuprops_icecone_a', 'Uwu icecream', 1),
+	('bzzz_uwuprops_icecone_b', 'Uwu icecream', 1),
+	('bzzz_uwuprops_icecone_c', 'Uwu icecream', 1),
+	('bzzz_uwuprops_icecone_d', 'Uwu icecream', 1),
+	('bzzz_uwuprops_cookie_a', 'Uwu Cookie', 1),	
+	('bzzz_uwuprops_coffee_a', 'Uwu Coffee', 1),
+	('bzzz_uwuprops_cappuccino_a', 'Uwu Cappuccino', 1),
+	('bzzz_uwuprops_drink_a', 'Uwu Ice coffee', 1),	
+	('bzzz_uwuprops_drink_b', 'Uwu Ice coffee', 1),
+	('bzzz_uwuprops_drink_c', 'Uwu Ice coffee', 1),	
+	('bzzz_uwuprops_drink_d', 'Uwu Ice coffee', 1)
+;
