3e798a1529c1ffc96c54b275427efb5e72510841
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 17:40:15 -0600

    mount zonah

---
+++ [mount_zonah]/gn_tplift_mountzonah/fxmanifest.lua
@@ -0,0 +1,36 @@
+shared_script ''
+shared_script ''
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+description "Teleport lift resource G&N's Studio"
+author "Dolu"
+version '1.0.0'
+
+client_scripts {
+   'config.lua',
+
+   'lib/rageui/RageUI.lua',
+   'lib/rageui/Menu.lua',
+   'lib/rageui/MenuController.lua',
+   'lib/rageui/components/*.lua',
+   'lib/rageui/elements/*.lua',
+   'lib/rageui/items/*.lua',
+
+   'client/client.lua',
+   'client/menu.lua'
+}
+
+escrow_ignore {
+   'config.lua',
+   'lib/rageui/RageUI.lua',
+   'lib/rageui/Menu.lua',
+   'lib/rageui/MenuController.lua',
+   'lib/rageui/components/*.lua',
+   'lib/rageui/elements/*.lua',
+   'lib/rageui/items/*.lua',
+   'client/client.lua',
+   'client/menu.lua'
+}
+dependency '/assetpacks'
