e7eaeec2a02f57d566ef90c28b735fbf749bdb2c
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 18:09:39 -0600

    MLOs

---
+++ tanaya-bagshop/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+
+author 'Tanaya'
+description 'Tanaya Bagshop'
+version '1.0.0'
+
+
+this_is_a_map 'yes'
+
+lua54 'yes'
+
+dependency '/assetpacks'
---
+++ tungao_salon/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game { 'gta5' }
+this_is_a_map 'yes'
+
+
+
+author 'DISCORD TUNGAO'
+description 'SALON SPA'
+version '1.0.0'
+dependency '/assetpacks'
