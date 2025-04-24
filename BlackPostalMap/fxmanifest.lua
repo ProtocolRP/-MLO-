fbbf67b6ef2d3a7970d01b7aaa995610b220e8bc
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 18:01:07 -0600

    black map

---
+++ BlackPostalMap/fxmanifest.lua
@@ -0,0 +1,12 @@
+shared_script '@fiveguard/'
+shared_script '@fiveguard/a'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheFlyBandit'
+description 'Custom Black Postal Map'
+discord 'https://discord.gg/theflybanditstudio'
+
+this_is_a_map 'yes'
+
+client_script "client.lua"
