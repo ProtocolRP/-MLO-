13e4d736e4a08d7d76b6b829a2b738d012a6d00a
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 18:06:28 -0600

    bahama mama

---
+++ marc_bahama_v3/fxmanifest.lua
@@ -0,0 +1,14 @@
+shared_script ''
+shared_script ''
+
+fx_version 'bodacious'
+game { 'gta5' }
+this_is_a_map "yes"
+lua54 'yes'
+
+client_scripts {
+	'particule.lua'
+}
+
+
+dependency '/assetpacks'
