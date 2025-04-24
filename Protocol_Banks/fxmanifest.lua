eed43b567d31c2467c3dd2b5fac7a889f0ae188e
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 18:08:05 -0600

    protocol bank

---
+++ Protocol_Banks/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version { 'cerulean' } games { 'gta5' } lua54 { 'yes' }
+
+name { 'Protocol Banks' }
+author { 'Assent Studios' }
+
+files {
+    'stream/*.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
+
+this_is_a_map 'yes'
