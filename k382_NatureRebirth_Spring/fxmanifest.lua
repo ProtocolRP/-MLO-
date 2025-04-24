c5e662ef35fb8ed7e4540ec072990d4b1481e337
ProtocolRP <gravityrp2024@gmail.com>
2025-04-23 18:05:44 -0600

    naturebirth

---
+++ k382_NatureRebirth_Spring/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'keihaan382'
+description 'k382_NatureRebirth_Spring'
+changelog 'Petals floating on the water of lakes and rivers'
+version '1.0.2'
+
+files {
+    'stream/[meta]/k382_props_custom_fx.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/[meta]/k382_props_custom_fx.ytyp'
+dependency '/assetpacks'
