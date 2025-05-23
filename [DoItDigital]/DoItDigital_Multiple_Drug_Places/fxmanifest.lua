2f50183b674a558c32e629cb4dec9dd9be0af340
ProtocolRP <gravityrp2024@gmail.com>
2025-05-23 13:14:25 -0600

    drug locations added

---
+++ [DoItDigital]/DoItDigital_Multiple_Drug_Places/fxmanifest.lua
@@ -0,0 +1,26 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'cerulean'
+author 'KilluaZoldyck#0099'
+description 'Drug Places'
+version '1.0.2'
+this_is_a_map 'yes'
+games { 'gta5' }
+dependency '/assetpacks'
+
+dependencies {
+    'DoItDigital_MLO_Assetpack',
+    'DoItDigital_Interiors'
+}
+  
+
+data_file 'DLC_ITYP_REQUEST' 'bkr_biker_dlc_int_ware01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bkr_mp_biker_coke.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'int_medical.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'hei_lev_des_mp_dlc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_minigame.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'bkr_mp_biker_weed.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'tr_dlc_int_01_tr.ytyp'
+
+
+
+dependency '/assetpacks'
---
+++ lokas_bunker_secret_room_FM/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games {'gta5'}
+escrow_ignore {
+  'stream/*.ymap',
+  'stream/*.ytyp',
+  'stream/*.ytd'
+}
+dependency '/assetpacks'
