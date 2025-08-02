b2d4f34ed2fd4c8e5b8909b13046d0ac12bff84f
ProtocolRP <gravityrp2024@gmail.com>
2025-08-02 11:35:20 -0400

    Billboards added

---
+++ [DoItDigital]/DoItDigital_Bunto_Avenue_Billboard/fxmanifest.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'cerulean'
+author 'KilluaZoldyck#0099'
+description 'Bunto Avenue Billboard'
+version '1.0.0'
+this_is_a_map 'yes'
+games { 'gta5' }
+dependency '/assetpacks'
+
+escrow_ignore {
+    'stream/central_gunshop_billboard.ydr',
+}
+dependency '/assetpacks'
