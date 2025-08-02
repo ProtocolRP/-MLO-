8d2c13a2f080237fac451f6d83e73c6eb47cb0be
ProtocolRP <gravityrp2024@gmail.com>
2025-08-02 19:26:58 -0400

    locksmith mlo

---
+++ Blockz Locksmith/blockz_locksmith/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map "yes"
+
+version '1.0'
+
+author 'Blockz Mapping'
+contact 'https://discord.gg/weHy5TtPFC'
+website 'https://blockz.tebex.io/'
+
+lua54 'yes'
+
+file 'interiorproxies.meta'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+dependency '/assetpacks'
