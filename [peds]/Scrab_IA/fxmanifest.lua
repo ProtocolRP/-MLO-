33f3817b2afd408300a6542aff5649c0bdffcfcf
ProtocolRP <gravityrp2024@gmail.com>
2025-09-08 21:18:01 -0400

    billboard fixes

---
+++ [peds]/Scrab_IA/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+files {
+    'peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+
+escrow_ignore {
+    'stream//.ytd',
+    'stream/.ymt',
+ 
+}
+dependency '/assetpacks'
+dependency '/assetpacks-redm'
