da582ad3b9dbbe52dd33f30c76b8cacbcd87eb4a
ProtocolRP <gravityrp2024@gmail.com>
2025-10-01 21:44:33 -0400

    added mc clubhouse

---
+++ clubhouse_vespucci/fxmanifest.lua
@@ -0,0 +1,23 @@
+lua54 'yes'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Apollo developments'
+description 'M.C vespucci'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+	'interiorproxies.meta'
+}
+
+
+escrow_ignore {
+    'stream/*.ytd',
+    'stream/club_vestrack.ydr'
+}
+
+dependency '/assetpacks'
