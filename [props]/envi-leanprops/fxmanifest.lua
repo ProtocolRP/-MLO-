80784acd82ad7ba3eea2fc201fba4297c0fa21b8
ProtocolRP <gravityrp2024@gmail.com>
2025-10-23 16:04:56 -0400

    added lean props

---
+++ [props]/envi-leanprops/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'Envi-Scripts'
+description 'Envi'
+version '1.0.0'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/envi-leanprops.ytyp'
+
+
+
+
+
+dependency '/assetpacks'
