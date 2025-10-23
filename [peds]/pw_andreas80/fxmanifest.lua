f3e95a66b2cf7ad154eb3d687919c2a536c0a2df
ProtocolRP <gravityrp2024@gmail.com>
2025-10-23 19:17:17 -0400

    added ped for al dente

---
+++ [peds]/pw_andreas80/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Pewe Studios <https://pewestudios.com>'
+description 'Peds FiveM By Pewe Studios'
+version '1.0.0'
+
+-- What to run
+files {
+	'peds.meta'
+}
+
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+dependency '/assetpacks'
