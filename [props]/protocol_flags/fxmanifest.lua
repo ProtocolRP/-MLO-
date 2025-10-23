4c108d3129b816868452037fa3652bb5b0d41416
ProtocolRP <gravityrp2024@gmail.com>
2025-10-23 16:05:32 -0400

    REMOVED PROPS

--- [props]/protocol_flags/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version 'cerulean'
-games {'gta5'}
-
-description 'Protocol Flags'
-author 'Assent Studios'
-
-this_is_a_map 'yes'
--- lokas_bunker_secret_room_FM/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-games {'gta5'}
-escrow_ignore {
-  'stream/*.ymap',
-  'stream/*.ytyp',
-  'stream/*.ytd'
-}
-dependency '/assetpacks'
