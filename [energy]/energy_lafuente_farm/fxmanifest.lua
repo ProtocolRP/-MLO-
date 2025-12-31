bc6d3368c544e4d3e1a94decc70a4ccd0367c0c9
ProtocolRP <gravityrp2024@gmail.com>
2025-12-31 15:23:37 -0500

    added la fuente farm

---
+++ [energy]/energy_lafuente_farm/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+
+this_is_a_map 'yes'
+
+lua54 'yes'
+
+
+
+
+file "stream/ymt/stables.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "stream/ymt/stables.ymt"
+dependency '/assetpacks'
